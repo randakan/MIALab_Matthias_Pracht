9f4bd30e6d71de2442827039bcd0d05d28767083
Matthias Pracht <matthias.pracht@students.unibe.ch>
2023-11-09 20:51:59 +0100

    C : better params

--- bin/main.py
+++ bin/main.py
@@ -73,8 +73,8 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
     labels_train = np.concatenate([img.feature_matrix[1] for img in images]).squeeze()
 
     forest = sk_ensemble.RandomForestClassifier(max_features=images[0].feature_matrix[0].shape[1],
-                                                n_estimators=100,
-                                                max_depth=200,
+                                                n_estimators=50,
+                                                max_depth=100,
                                                 random_state=42)
 
     start_time = timeit.default_timer()
