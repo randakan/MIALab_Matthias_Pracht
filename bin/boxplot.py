197e7948bba409b81501ccd397c978a53274a7d6
Yves Moerlen <yves.moerlen@students.unibe.ch>
2023-11-22 15:12:23 +0100

    C : Dr Matthias hets so wölle

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -9,26 +9,28 @@ def main():
     # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
     #  in a boxplot
 
-    fig, axs = plt.subplots(2, 1)
+    fig, axs = plt.subplots(1, 2)
     fontsize = 10
 
     # Figure Dice
     path = 'mia-result/'
     folder = '2023-11-22-09-49-00'
     file = pd.read_csv(path + folder + '/results_DICE.csv', sep=';')
-    aplot = file.boxplot(column='DICE', by='LABEL', fontsize=fontsize, ax=axs.flatten()[0], return_type='axes')
+    a_plot = file.boxplot(column='DICE', by='LABEL', fontsize=fontsize, ax=axs.flatten()[0], return_type='axes')
     plt.suptitle('')
-    aplot[0].set_title('Dice Values of ' + folder, fontsize=fontsize+2)
-    aplot[0].set_xlabel('Brain structure', fontsize=fontsize)
-    aplot[0].set_ylabel('Dice score', fontsize=fontsize)
+    a_plot[0].set_title('Dice Values of ' + folder, fontsize=fontsize + 2)
+    a_plot[0].set_xlabel('Brain structure', fontsize=fontsize)
+    a_plot[0].set_ylabel('Dice score', fontsize=fontsize)
+    a_plot[0].set_ylim([0, 1])
 
     # Figure Hausdorff
     file = pd.read_csv(path + folder + '/results_HSDRF.csv', sep=';')
-    bplot = file.boxplot(column='HDRFDST', by='LABEL', fontsize=fontsize, ax=axs.flatten()[1], return_type='axes')
+    b_plot = file.boxplot(column='HDRFDST', by='LABEL', fontsize=fontsize, ax=axs.flatten()[1], return_type='axes')
     plt.suptitle('')
-    bplot[0].set_title('Hausdorff distance of ' + folder, fontsize=fontsize+2)
-    bplot[0].set_xlabel('Brain structure', fontsize=fontsize)
-    bplot[0].set_ylabel('Hausdorff distance', fontsize=fontsize)
+    b_plot[0].set_title('Hausdorff distance of ' + folder, fontsize=fontsize + 2)
+    b_plot[0].set_xlabel('Brain structure', fontsize=fontsize)
+    b_plot[0].set_ylabel('Hausdorff distance', fontsize=fontsize)
+    b_plot[0].set_ylim([0, 20])
 
     plt.tight_layout()
     plt.show()
