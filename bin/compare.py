3a0b395dbb3154251d5b648732b8d4624cd871cd
Matthias Pracht <pracht.matthias@gmail.com>
2023-12-04 12:40:00 +0100

    C : added data_archive for valid data

--- bin/compare.py
+++ bin/compare.py
@@ -21,7 +21,7 @@ def main():
 
     # Dropdown menu options
     list_of_files = glob.glob("mia-result/*")  # * means all, if you need specific format then *.csv
-    latest_file = os.path.basename(max(list_of_files, key=os.path.getctime))
+    latest_file = max(list_of_files, key=os.path.getctime)
     baseline = "baseline"
     options = [baseline] + list_of_files
 
@@ -71,7 +71,7 @@ def readFiles(completePath):
     fontsize = 10
 
     # Figure Dice
-    file = pd.read_csv(path + folder + '/results_DICE.csv', sep=';')
+    file = pd.read_csv('./' + path + '/' + folder + '/results_DICE.csv', sep=';')
     a_plot = file.boxplot(column='DICE', by='LABEL', fontsize=fontsize, ax=axs.flatten()[0], return_type='axes')
     plt.suptitle('')
     a_plot[0].set_title('Dice Values of ' + folder, fontsize=fontsize + 2)
@@ -80,7 +80,7 @@ def readFiles(completePath):
     a_plot[0].set_ylim([0, 1])
 
     # Figure Hausdorff
-    file = pd.read_csv(path + folder + '/results_HSDRF.csv', sep=';')
+    file = pd.read_csv('./' + path + '/' + folder + '/results_HSDRF.csv', sep=';')
     b_plot = file.boxplot(column='HDRFDST', by='LABEL', fontsize=fontsize, ax=axs.flatten()[1], return_type='axes')
     plt.suptitle('')
     b_plot[0].set_title('Hausdorff distance of ' + folder, fontsize=fontsize + 2)
